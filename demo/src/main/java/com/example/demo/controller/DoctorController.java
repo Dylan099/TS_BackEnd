8e5ce4fe7a0ec1f6df001ae51e33f558cfcfdc06
Cristopher Castro <castro.inofuentes.cristopher@gmail.com>
2020-09-30 18:04:43 -0400

    Doctor Controller

--- demo/src/main/java/com/example/demo/controller/DoctorController.java
+++ demo/src/main/java/com/example/demo/controller/DoctorController.java
@@ -5,10 +5,13 @@ import com.example.demo.bl.DoctorBl;
 import com.example.demo.domain.PacienteEntity;
 import com.itextpdf.text.DocumentException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -29,20 +32,26 @@ public class DoctorController {
 
     }
 
-
-    @RequestMapping("/listaPacientes")
+    @RequestMapping("/listpatient")
     public List<PacienteEntity> create_pacientes_list() {
         //Recupera los datos de los pacientes del doctor con el id ""
-        List<PacienteEntity> pacienteDtoList= doctorBl.create_pacientes_list(1);
+        List<PacienteEntity> pacienteDtoList= doctorBl.create_pacientes_list(5);
         return pacienteDtoList;
 
     }
-    @RequestMapping("/listaPacientesPDF")
-    public String create_pdf_pacientes_list() throws FileNotFoundException, DocumentException {
+
+    @RequestMapping("/listpatientPDF")
+    public ResponseEntity create_pdf_pacientes_list() throws IOException, DocumentException {
         //Recupera los datos de los pacientes del doctor con el id "" y los guarda en un pdf
         doctorBl.create_pdf_pacientes_list(1);
-        return "Creado";
+        return new ResponseEntity(new DoctorController.Mensaje("Creado"), HttpStatus.ACCEPTED);
 
     }
 
+    public static class Mensaje{
+        public String mensaje;
+        public Mensaje(String mensaje){
+            this.mensaje = mensaje;
+        }
+    }
 }
